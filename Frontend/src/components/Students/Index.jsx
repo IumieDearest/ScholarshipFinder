4ca5da8b21949ff915fdd4a7c70aea9cc16bc296
IumieDearest <annamaeregis656@gmail.com>
2026-04-19 15:41:04 +0800

    first commit

---
+++ Frontend/src/components/Students/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"; 
+const StudentsIndex = () => {
+  return (
+    <div>
+      <h1>Students Index</h1>
+    </div>
+  );
+};
+
+export default StudentsIndex;
